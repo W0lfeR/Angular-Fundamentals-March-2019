cc1d6364c3194a0777c1d2d270fea19368ac477f
W0lfeR <33635203+W0lfeR@users.noreply.github.com>
2019-04-16 22:50:58 +0300

    update files

--- angular-skyline-technology-group-project/skyline-technology-group/src/app/app-routing.module.ts
+++ angular-skyline-technology-group-project/skyline-technology-group/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
+
+// Components
 import { HomeComponent } from './components/home/home.component';
 import { SigninComponent } from './components/authentication/signin/signin.component';
 import { SignupComponent } from './components/authentication/signup/signup.component';
-import {AuthGuard} from './components/authentication/guards/auth.guard';
-import {AdminGuard} from './components/authentication/guards/admin.guard';
+
 const routes: Routes = [  
 { path: '', pathMatch: 'full', component: HomeComponent },
   { path: 'home', component: HomeComponent },
--- angular-skyline-technology-group-project/skyline-technology-group/src/app/app.module.ts
+++ angular-skyline-technology-group-project/skyline-technology-group/src/app/app.module.ts
@@ -1,18 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
-import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { SigninComponent } from './components/authentication/signin/signin.component';
 import { SignupComponent } from './components/authentication/signup/signup.component';
+import { HomeComponent } from './components/home/home.component';
 import { DropdownDirective } from './components/navbar/dropdown.directive';
 import { CollapseDirective } from './components/navbar/collapse.directive';
+
+
 import { AuthService } from './components/authentication/auth.service';
-import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
@@ -20,16 +21,17 @@ import { HomeComponent } from './components/home/home.component';
     NavbarComponent,
     SigninComponent,
     SignupComponent,
+    HomeComponent,
     DropdownDirective,
     CollapseDirective,
-    HomeComponent,
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpClientModule,
+    HttpClientModule
   ],
   providers: [AuthService,HttpClientModule],
   bootstrap: [AppComponent]
